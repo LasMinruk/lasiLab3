e528f6d5b741ed28d332954d10a5ec6ff241e7c7
lasiru Minruk <lasiru2225@gmail.com>
2025-03-14 02:00:38 +0530

    Hello World from Node.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-const fs = require('fs');  // Add this line at the beginning
+const http = require('http');
 
-fs.writeFile('file.txt', 'Hello World from Node.js!', (err) => {
-    if (err) throw err;
-    console.log('File written successfully!');
-});
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>Hello, this is my Node.js server!</h1>');
+    res.end();
+}).listen(8080);
+
+console.log("Server running at http://localhost:8080");
